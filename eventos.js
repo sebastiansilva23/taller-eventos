8ef883da22d58daed3a17310962eabc4d1f3e507
sebastiansilva23 <ssebasss20323@gmail.com>
2023-08-09 20:53:32 -0300

    Solucione el problema de las alertas juntas

--- eventos.js
+++ eventos.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const miDiv = document.getElementById("miDiv");
+    const btnSaludar = document.getElementById("miBoton");
+
     miDiv.addEventListener("click", function() {
-        alert("Hola! Soy el div");
+        alert("Hola, soy el div!");
+    });
+
+    btnSaludar.addEventListener("click", function() {
+        alert("Hola desde el botón!");
     });
 });
