b40577891c9b5bc52ea94c1637c89aa34cb6e4bf
sebastiansilva23 <ssebasss20323@gmail.com>
2023-08-09 19:27:02 -0300

    cree un archivo js y uno css, agregue evento saludar del div y le di estilo al mismo div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const miDiv = document.getElementById("miDiv");
+    miDiv.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
